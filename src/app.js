01fe6d8aaf57b4496ea7dc8a8ec809231888f2bb
Alexander Ottenhoff <alexander@sqreen.io>
2017-10-13 18:23:45 +0200

    Replaced useless component with if forest

--- src/app.js
+++ src/app.js
@@ -16,37 +16,33 @@ class App extends React.Component {
   }
 
   render() {
-    return (
-      <div>
-        {this.state.playing ? (
-          <div>
-            {this.state.players.size > 1 ? (
-              <Round
-                number={this.state.round}
-                players={this.state.players.take(this.state.playersPerGame)}
-                onClick={index =>
-                  this.setState(({ players, playersPerGame, round }) => ({
-                    players: players
-                      .skip(playersPerGame)
-                      .concat(players.take(playersPerGame).remove(index)),
-                    round: round + 1,
-                  }))}
-              />
-            ) : (
-              <div>A winner is {this.state.players.get(0)}</div>
-            )}
-          </div>
-        ) : (
-          <PlayersEntry
-            onStart={({ players, playersPerGame }) =>
-              this.setState({
-                players,
-                playersPerGame,
-                playing: true,
-              })}
+    if (this.state.playing) {
+      if (this.state.players.size > 1) {
+        return (
+          <Round
+            number={this.state.round}
+            players={this.state.players.take(this.state.playersPerGame)}
+            onClick={index =>
+              this.setState(({ players, playersPerGame, round }) => ({
+                players: players
+                  .skip(playersPerGame)
+                  .concat(players.take(playersPerGame).remove(index)),
+                round: round + 1,
+              }))}
           />
-        )}
-      </div>
+        );
+      }
+      return <div>A winner is {this.state.players.get(0)}</div>;
+    }
+    return (
+      <PlayersEntry
+        onStart={({ players, playersPerGame }) =>
+          this.setState({
+            players,
+            playersPerGame,
+            playing: true,
+          })}
+      />
     );
   }
 }
